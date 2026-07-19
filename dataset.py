10fa52ae0491e8352a9521a96392347825aa8ea5
Azamat E. Zakirovich <azamateshqoziyev2@gmail.com>
2026-07-19 20:12:02 +0500

    train dataset transformation part is finished.

--- dataset.py
+++ dataset.py
@@ -5,4 +5,29 @@ import config
 
 
 class Dataset:
-    ...
+
+    def __init__(self):
+
+        # transform train dataset elements
+        self.train_transform = transforms.Compose([
+
+            # resize the image
+            transforms.Resize(
+                (config.image_size, config.image_size)
+            ),
+
+            # to avoid underfitting or overfitting I am going to flip half of dataset elements
+            transforms.RandomVerticalFlip(p=0.5),
+
+            # main reason for rotate the image is, avoid underfitting or overfitting
+            transforms.RandomRotation(degrees=10),
+
+            # to teach my cnn, I need to change dataset elements to tensors
+            transforms.ToTensor(),
+
+            # normalization
+            transforms.Normalize(
+                mean = (0.485, 0.456, 0.406),
+                std = (0.229, 0.224, 0.225)
+            )
+        ])
