92da2f7638efd47f4b7cc9de057ecd9b69e5a069
Azamat E. Zakirovich <azamateshqoziyev2@gmail.com>
2026-07-20 12:47:18 +0500

    Convolutional layers initialized.

--- model/cnn.py
+++ model/cnn.py
@@ -3,4 +3,41 @@ import torch.nn as nn
 
 
 class CNN(nn.Module):
-    ...
+
+    def __init__(self):
+        super(CNN, self).__init__()
+
+        self.conv1 = nn.Conv2d(
+            in_channels=3,
+            out_channels=32,
+            kernel_size=3,
+            padding=1,
+        )
+
+        self.bn1 = nn.BatchNorm2d(32)
+
+
+        self.conv2 = nn.Conv2d(
+            in_channels=32,
+            out_channels=64,
+            kernel_size=3,
+            padding=1,
+        )
+
+        self.bn2 = nn.BatchNorm2d(64)
+
+        self.conv3 = nn.Conv2d(
+            in_channels=64,
+            out_channels=128,
+            kernel_size=3,
+            padding=1,
+        )
+
+        self.bn3 = nn.BatchNorm2d(128)
+
+        self.conv4 = nn.Conv2d(
+            in_channels=128,
+            out_channels=256,
+            kernel_size=3,
+            padding=1,
+        )
